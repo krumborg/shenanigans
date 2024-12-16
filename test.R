8a766f8053e66c8abdadd8b8c515a8d498f89a6d
Julie Krumborg <julierudbech@gmail.com>
2024-12-16 16:44:10 +0100

    Added information on reprex - how to share parts of your code to get help with troubleshooting

--- test.R
+++ test.R
@@ -1,1 +1,16 @@
 # Test script
+# Telling stories with data notes
+# Julie Krumborg
+
+## Reproducibility
+
+### Asking for help with errors
+
+# 1. Reprex
+library(reprex) #library that helps with highlighting trouble-code to share
+ 
+# 2. Highlight problematic code
+
+# 3. Run reprex()
+reprex()
+# If the code is self-contained, then it will preview in the viewer. If it is not, then it will error, and you should rewrite the code so that it is self-contained.
